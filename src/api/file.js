35da4e7d8ca7c69b3a737cceff6592dbdc9569ba
kaverinstudio <kaverinstudio.gmail.com>
2023-04-23 17:39:18 +0600

    bag fixing

--- src/api/file.js
+++ src/api/file.js
@@ -2,153 +2,198 @@ import axios from "axios";
 import {API_URL} from "./config";
 import {
     addFile,
-    changeUploadFile, clearUploadFiles, deleteUploadFiles, errorFileMessages, hideUploader, setConfirmOrder,
+    changeUploadFile,
+    deleteUploadFiles,
+    errorFileMessages,
+    hideUploader,
+    setConfirmOrder,
     setDeleteFile,
     setFileInitial,
     setFileInitialProps,
     showUploader,
-    uploadFiles
+    uploadFiles,
 } from "../redux/slices/fileSlice";
 import {errorMessages} from "../redux/slices/authSlice";
 import keygen from "keygenerator";
 
 export const getFiles = (user) => {
-    return async dispatch => {
+    return async (dispatch) => {
         try {
-            let config = null
-            if (user){
-                config = {headers:{Authorization: `Token ${localStorage.getItem('token')}`}}
+            let config = null;
+            if (user) {
+                config = {
+                    headers: {Authorization: `Token ${localStorage.getItem("token")}`},
+                };
             }
             const response = await axios.get(`${API_URL}api/print/`, config);
             const data = response.data.files;
-            const papierSize = response.data.papier_size
-            const papierType = response.data.papier_type
-            const services = response.data.services
-            dispatch(setFileInitial(data))
-            dispatch(setFileInitialProps({'papierSize': papierSize, 'papierType': papierType, 'services': services}))
-        }catch (e) {
-            dispatch(e.response.messages.data)
+            const papierSize = response.data.papier_size;
+            const papierType = response.data.papier_type;
+            const services = response.data.services;
+            dispatch(setFileInitial(data));
+            dispatch(
+                setFileInitialProps({
+                    papierSize: papierSize,
+                    papierType: papierType,
+                    services: services,
+                })
+            );
+        } catch (e) {
+            dispatch(e.response.messages.data);
         }
-    }
-}
-
+    };
+};
 
 export const fileUpload = (file, user) => {
-    return async dispatch => {
+    return async (dispatch) => {
         try {
-            let session_key = localStorage.getItem('session_key')
-            if (!session_key){
-                session_key = keygen.session_id()
-                localStorage.setItem('session_key', session_key)
+            let session_key = localStorage.getItem("session_key");
+            if (!session_key) {
+                session_key = keygen.session_id();
+                localStorage.setItem("session_key", session_key);
             }
 
-            const formData = new FormData()
-            formData.append('file', file)
-            formData.append('session_key', session_key)
+            const formData = new FormData();
+            formData.append("file", file);
+            formData.append("session_key", session_key);
 
-            const uploadFile = {name: file.name, progress: 0, id: Date.now()}
-            dispatch(uploadFiles(uploadFile))
+            const uploadFile = {name: file.name, progress: 0, id: Date.now()};
+            dispatch(uploadFiles(uploadFile));
 
             let config = {
-                headers:{Authorization: `Token ${localStorage.getItem('token')}`},
-                onUploadProgress: progressEvent => {
-
-                    const totalLength = progressEvent.total
-                    if (totalLength){
-                        uploadFile.progress = Math.round((progressEvent.loaded * 100) / totalLength)
-                        dispatch(changeUploadFile(uploadFile))
-                        if (uploadFile.progress === 100){
-                            dispatch(deleteUploadFiles(uploadFile))
+                headers: {Authorization: `Token ${localStorage.getItem("token")}`},
+                onUploadProgress: (progressEvent) => {
+                    const totalLength = progressEvent.total;
+                    if (totalLength) {
+                        uploadFile.progress = Math.round(
+                            (progressEvent.loaded * 100) / totalLength
+                        );
+                        dispatch(changeUploadFile(uploadFile));
+                        if (uploadFile.progress === 100) {
+                            dispatch(deleteUploadFiles(uploadFile));
                         }
                     }
-                }
-            }
-            if (!user){
+                },
+            };
+            if (!user) {
                 config = {
-                    withCredentials: true,
-                    onUploadProgress: progressEvent => {
-
-                        const totalLength = progressEvent.total
-                        if (totalLength){
-                            uploadFile.progress = Math.round((progressEvent.loaded * 100) / totalLength)
-                            dispatch(changeUploadFile(uploadFile))
-                            if (uploadFile.progress === 100){
-                                dispatch(deleteUploadFiles(uploadFile))
+                    headers: {"X-CSRFToken": localStorage.getItem("session_key")},
+                    onUploadProgress: (progressEvent) => {
+                        const totalLength = progressEvent.total;
+                        if (totalLength) {
+                            uploadFile.progress = Math.round(
+                                (progressEvent.loaded * 100) / totalLength
+                            );
+                            dispatch(changeUploadFile(uploadFile));
+                            if (uploadFile.progress === 100) {
+                                dispatch(deleteUploadFiles(uploadFile));
                             }
                         }
-                    }
-                }
+                    },
+                };
             }
-            const response = await axios.post(`${API_URL}api/print/upload`, formData, config)
-            dispatch(addFile(response.data.file))
-        }catch (e) {
-            dispatch(errorMessages(e.response.data))
+            const response = await axios.post(
+                `${API_URL}api/print/upload`,
+                formData,
+                config
+            );
+            dispatch(addFile(response.data.file));
+        } catch (e) {
+            dispatch(errorMessages(e.response.data));
         }
-    }
-}
+    };
+};
 
 export const fileUpdate = (format, papier, count, fileId, user) => {
-    return async dispatch => {
+    return async (dispatch) => {
         try {
-
-            let config = {headers:{Authorization: `Token ${localStorage.getItem('token')}`}}
-            if (!user){
-                config = { withCredentials: true }
+            let config = {
+                headers: {Authorization: `Token ${localStorage.getItem("token")}`},
+            };
+            if (!user) {
+                config = {
+                    headers: {"X-CSRFToken": localStorage.getItem("session_key")},
+                };
             }
 
-            const response = await axios.put(`${API_URL}api/print/update/${fileId}`, {
-                format,
-                papier,
-                count
-            }, config);
-            dispatch(setFileInitial(response.data.files))
-
-        }catch (e) {
-            dispatch(errorMessages(e.response.data))
+            const response = await axios.put(
+                `${API_URL}api/print/update/${fileId}`,
+                {
+                    format,
+                    papier,
+                    count,
+                },
+                config
+            );
+            dispatch(setFileInitial(response.data.files));
+        } catch (e) {
+            dispatch(errorMessages(e.response.data));
         }
-    }
-}
+    };
+};
 
 export const fileDelete = (user, fileId) => {
-    return async dispatch => {
+    return async (dispatch) => {
         try {
-            let config = {headers:{Authorization: `Token ${localStorage.getItem('token')}`}}
-            if (!user){
-                config = { withCredentials: true }
+            let config = {
+                headers: {Authorization: `Token ${localStorage.getItem("token")}`},
+            };
+            if (!user) {
+                config = {
+                    headers: {"X-CSRFToken": localStorage.getItem("session_key")},
+                };
             }
-            await axios.delete(`${API_URL}api/print/delete/${fileId}`, config)
-            dispatch(setDeleteFile(fileId))
-        }catch (e) {
-            dispatch(errorMessages(e.response.data))
+            await axios.delete(`${API_URL}api/print/delete/${fileId}`, config);
+            dispatch(setDeleteFile(fileId));
+        } catch (e) {
+            dispatch(errorMessages(e.response.data));
         }
-    }
-}
+    };
+};
 
-export const confirmOrder = (user, first_name, last_name, phone, email, address, comments, delivery, order) => {
-    return async dispatch => {
+export const confirmOrder = (
+    user,
+    first_name,
+    last_name,
+    phone,
+    email,
+    address,
+    comments,
+    delivery,
+    order
+) => {
+    return async (dispatch) => {
         try {
-            dispatch(showUploader())
-            let config = {headers:{Authorization: `Token ${localStorage.getItem('token')}`}}
-            if (!user){
-                config = { withCredentials: true }
+            dispatch(showUploader());
+            let config = {
+                headers: {Authorization: `Token ${localStorage.getItem("token")}`},
+            };
+            if (!user) {
+                config = {
+                    headers: {"X-CSRFToken": localStorage.getItem("session_key")},
+                };
             }
-            const response = await axios.post(`${API_URL}api/print/confirm`, {
-                first_name,
-                last_name,
-                phone,
-                email,
-                address,
-                comments,
-                delivery,
-                order
-            }, config)
-            dispatch(setConfirmOrder(response.data.order))
-            dispatch(setFileInitial(response.data.files))
-            dispatch(errorFileMessages('ok'))
-            dispatch(hideUploader())
-        }catch (e) {
-            dispatch(errorMessages(e.response.data))
-            dispatch(hideUploader())
+            const response = await axios.post(
+                `${API_URL}api/print/confirm`,
+                {
+                    first_name,
+                    last_name,
+                    phone,
+                    email,
+                    address,
+                    comments,
+                    delivery,
+                    order,
+                },
+                config
+            );
+            dispatch(setConfirmOrder(response.data.order));
+            dispatch(setFileInitial(response.data.files));
+            dispatch(errorFileMessages("ok"));
+            dispatch(hideUploader());
+        } catch (e) {
+            dispatch(errorMessages(e.response.data));
+            dispatch(hideUploader());
         }
-    }
-}
+    };
+};
--- src/api/products.js
+++ src/api/products.js
@@ -1,159 +1,210 @@
 import axios from "axios";
-import { API_URL } from './config'
+import {API_URL} from "./config";
 import keygen from "keygenerator";
 import {
-    errorProductMessages,
-    getCart, loadInitialProducts,
-    loadPhotos,
-    loadProducts,
+  errorProductMessages,
+  getCart,
+  loadInitialProducts,
+  loadPhotos,
+  loadProducts,
 } from "../redux/slices/productSlice";
-import {errorFileMessages, hideUploader, setConfirmOrder, showUploader} from "../redux/slices/fileSlice";
+import {errorFileMessages, hideUploader, setConfirmOrder, showUploader,} from "../redux/slices/fileSlice";
 import {errorMessages} from "../redux/slices/authSlice";
 
-
-export const getProducts = (category='', slug= '', minPrice, maxPrice, sort, manufactured, cat) => {
-    return async dispatch => {
-        let categoryUrl = ''
-        let slugUrl = ''
-        if (category){
-            categoryUrl = `${category}/`
-        }
-        if (slug){
-            slugUrl = `${slug}/`
-        }
-        try {
-            dispatch(showUploader())
-            const response = await axios.get(`${API_URL}api/products/${categoryUrl}${slugUrl}`, {
-                params: {
-                    minPrice: minPrice,
-                    maxPrice: maxPrice,
-                    sort: sort,
-                    manufactured: manufactured,
-                    cat: cat
-                }
-            })
-            dispatch(loadProducts(response.data.products))
-            dispatch(loadPhotos(response.data.photos))
-            if (!minPrice && !maxPrice){
-                dispatch(loadInitialProducts(response.data.products))
-            }
-            dispatch(hideUploader())
-        }catch (e) {
-            console.log(e)
-        }
+export const getProducts = (
+    category = "",
+    slug = "",
+    minPrice,
+    maxPrice,
+    sort,
+    manufactured,
+    cat
+) => {
+  return async (dispatch) => {
+    let categoryUrl = "";
+    let slugUrl = "";
+    if (category) {
+      categoryUrl = `${category}/`;
     }
-}
+    if (slug) {
+      slugUrl = `${slug}/`;
+    }
+    try {
+      dispatch(showUploader());
+      const response = await axios.get(
+          `${API_URL}api/products/${categoryUrl}${slugUrl}`,
+          {
+            params: {
+              minPrice: minPrice,
+              maxPrice: maxPrice,
+              sort: sort,
+              manufactured: manufactured,
+              cat: cat,
+            },
+          }
+      );
+      dispatch(loadProducts(response.data.products));
+      dispatch(loadPhotos(response.data.photos));
+      if (!minPrice && !maxPrice) {
+        dispatch(loadInitialProducts(response.data.products));
+      }
+      dispatch(hideUploader());
+    } catch (e) {
+      console.log(e);
+    }
+  };
+};
 
 export const addProductToCart = (product, user, count) => {
-    return async dispatch => {
-        try {
-            let session_key = localStorage.getItem('session_key')
-            if (!session_key){
-                session_key = keygen.session_id()
-                localStorage.setItem('session_key', session_key)
-            }
-            let config = {
-                headers: {Authorization: `Token ${localStorage.getItem('token')}`}
-            }
-            if (!user){
-                config = {
-                    withCredentials: true
-                }
-            }
-            const response = await axios.post(`${API_URL}api/cart/create/`, {
-                product,
-                count,
-                session_key
-            }, config)
-            dispatch(getCart(response.data))
-        }catch (e) {
-            dispatch(errorProductMessages(e))
-        }
+  return async (dispatch) => {
+    try {
+      let session_key = localStorage.getItem("session_key");
+      if (!session_key) {
+        session_key = keygen.session_id();
+        localStorage.setItem("session_key", session_key);
+      }
+      let config = {
+        headers: {Authorization: `Token ${localStorage.getItem("token")}`},
+      };
+      if (!user) {
+        config = {
+          headers: {"X-CSRFToken": session_key},
+        };
+      }
+      const response = await axios.post(
+          `${API_URL}api/cart/create/`,
+          {
+            product,
+            count,
+            session_key,
+          },
+          config
+      );
+      dispatch(getCart(response.data));
+    } catch (e) {
+      dispatch(errorProductMessages(e));
     }
-}
+  };
+};
 
 export const getUserCart = () => {
-  return async dispatch => {
-      try {
-          const response = await axios.get(`${API_URL}api/cart/`)
-          dispatch(getCart(response.data))
-      }catch (e) {
-          console.log(e)
-      }
-  }
-}
+  return async (dispatch) => {
+    try {
+      const response = await axios.get(`${API_URL}api/cart/`);
+      dispatch(getCart(response.data));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+};
 
 export const updateUserCart = (user, id, count) => {
-    return async dispatch => {
-        try {
-            let config = {headers:{Authorization: `Token ${localStorage.getItem('token')}`}}
-            if (!user){
-                config = { withCredentials: true }
-            }
-            const response = await axios.put(`${API_URL}api/cart/update/${id}`,{count}, config)
-            dispatch(getCart(response.data))
-        }catch (e) {
-            console.log(e)
-        }
+  return async (dispatch) => {
+    try {
+      let config = {
+        headers: {Authorization: `Token ${localStorage.getItem("token")}`},
+      };
+      if (!user) {
+        config = {
+          headers: {"X-CSRFToken": localStorage.getItem("session_key")},
+        };
+      }
+      const response = await axios.put(
+          `${API_URL}api/cart/update/${id}`,
+          {count},
+          config
+      );
+      dispatch(getCart(response.data));
+    } catch (e) {
+      console.log(e);
     }
-}
+  };
+};
 
 export const deleteUserCart = (user, id) => {
-    return async dispatch => {
-        try {
-            let config = {headers:{Authorization: `Token ${localStorage.getItem('token')}`}}
-            if (!user){
-                config = { withCredentials: true }
-            }
-            const response = await axios.delete(`${API_URL}api/cart/delete/${id}`, config)
-            dispatch(getCart(response.data))
-        }catch (e){
-            console.log(e)
-        }
+  return async (dispatch) => {
+    try {
+      let config = {
+        headers: {Authorization: `Token ${localStorage.getItem("token")}`},
+      };
+      if (!user) {
+        config = {
+          headers: {"X-CSRFToken": localStorage.getItem("session_key")},
+        };
+      }
+      const response = await axios.delete(
+          `${API_URL}api/cart/delete/${id}`,
+          config
+      );
+      dispatch(getCart(response.data));
+    } catch (e) {
+      console.log(e);
     }
-}
+  };
+};
 
 export const deleteAllProductInCart = (user) => {
-    return async dispatch => {
-        try {
-            let config = {headers:{Authorization: `Token ${localStorage.getItem('token')}`}}
-            if (!user){
-                config = { withCredentials: true }
-            }
-            const response = await axios.delete(`${API_URL}api/cart/delete/`, config)
-            dispatch(getCart(response.data))
-        }catch (e) {
-            console.log(e)
-        }
+  return async (dispatch) => {
+    try {
+      let config = {
+        headers: {Authorization: `Token ${localStorage.getItem("token")}`},
+      };
+      if (!user) {
+        config = {
+          headers: {"X-CSRFToken": localStorage.getItem("session_key")},
+        };
+      }
+      const response = await axios.delete(`${API_URL}api/cart/delete/`, config);
+      dispatch(getCart(response.data));
+    } catch (e) {
+      console.log(e);
     }
-}
+  };
+};
 
-export const shopConfirmOrder = (user, first_name, last_name, phone, email, address, comments, delivery, order) => {
-    return async dispatch => {
-        try {
-            dispatch(showUploader())
-            let config = {headers:{Authorization: `Token ${localStorage.getItem('token')}`}}
-            if (!user){
-                config = { withCredentials: true }
-            }
-            const response = await axios.post(`${API_URL}api/cart/confirm`, {
-                first_name,
-                last_name,
-                phone,
-                email,
-                address,
-                comments,
-                delivery,
-                order
-            }, config)
-            dispatch(setConfirmOrder(response.data.order))
-            dispatch(getCart(null))
-            dispatch(errorFileMessages('ok'))
-            dispatch(hideUploader())
-        }catch (e) {
-            dispatch(errorMessages(e.response.data))
-            dispatch(hideUploader())
-        }
+export const shopConfirmOrder = (
+    user,
+    first_name,
+    last_name,
+    phone,
+    email,
+    address,
+    comments,
+    delivery,
+    order
+) => {
+  return async (dispatch) => {
+    try {
+      dispatch(showUploader());
+      let config = {
+        headers: {Authorization: `Token ${localStorage.getItem("token")}`},
+      };
+      if (!user) {
+        config = {
+          headers: {"X-CSRFToken": localStorage.getItem("session_key")},
+        };
+      }
+      const response = await axios.post(
+          `${API_URL}api/cart/confirm`,
+          {
+            first_name,
+            last_name,
+            phone,
+            email,
+            address,
+            comments,
+            delivery,
+            order,
+          },
+          config
+      );
+      dispatch(setConfirmOrder(response.data.order));
+      dispatch(getCart(null));
+      dispatch(errorFileMessages("ok"));
+      dispatch(hideUploader());
+    } catch (e) {
+      dispatch(errorMessages(e.response.data));
+      dispatch(hideUploader());
     }
-  
-}
+  };
+};
